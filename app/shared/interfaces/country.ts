5d360f3324288fb5822cec056bb294b91e111816
George Kamtziridis <georgekam96@gmail.com>
2017-07-06 13:49:51 +0300

    Added CountryName interface

--- app/shared/interfaces/country.ts
+++ app/shared/interfaces/country.ts
@@ -13,3 +13,20 @@ export interface IEmergencyPhoneNumber {
     Number: string;
     LastUpdate: string;
 }
+
+export interface ICountryName {
+    as: string;
+    city: string;
+    country: string;
+    countryCode: string;
+    isp: string;
+    lat: number;
+    lon: number;
+    org: string;
+    query: string;
+    region: string;
+    regionName: string;
+    status: string;
+    timezone: string;
+    zip: string;
+}
