d85c012545a333b888ab2a7f17707dc0860ae285
George Kamtziridis <georgekam96@gmail.com>
2017-07-06 13:46:54 +0300

    Added the Country and EmergencyPhoneNumber interfaces

---
+++ app/shared/interfaces/country.ts
@@ -0,0 +1,15 @@
+export interface ICountry {
+    ID: string;
+    Name: string;
+    Language: string;
+    Responsiveness: string;
+    LastUpdate: string;
+    SimNeeded112: string;
+    OnlyNumber: string;
+}
+
+export interface IEmergencyPhoneNumber {
+    Type: string;
+    Number: string;
+    LastUpdate: string;
+}
