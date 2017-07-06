76a002d39e7a6a1cf61106f848a69ba26057ea4c
George Kamtziridis <georgekam96@gmail.com>
2017-07-06 13:55:15 +0300

    Implemented interfaces

--- app/shared/country.service.ts
+++ app/shared/country.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { ICountry, IEmergencyPhoneNumber, ICountryName } from './interfaces/country';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -8,27 +9,27 @@ export class CountryService {
 
     constructor(private _http: Http){}
 
-    getCountryName(ip: string): object {
+    getCountryName(ip: string): Observable<ICountryName> {
         return this._http
                     .get('http://ip-api.com/json/' + ip)
-                    .map((response: Response) => response.json());
+                    .map((response: Response) => <ICountryName>response.json());
     }
 
-    getCountries(): Observable<object[]> {
+    getCountries(): Observable<ICountry[]> {
         return this._http
                     .get('http://83.212.115.201/api.php/country')
-                    .map((response: Response) => response.json());
+                    .map((response: Response) => <ICountry[]>response.json());
     }
-    getCountry(country: string): Observable<object> {
+    getCountry(country: string): Observable<ICountry> {
         return this._http
                     .get('http://83.212.115.201/api.php/country/' + country)
-                    .map((response: Response) => response.json());
+                    .map((response: Response) => <ICountry>response.json());
     }
 
-    getEmergencyPhoneNumbers(country: string): Observable<object[]> {
+    getEmergencyPhoneNumbers(country: string): Observable<IEmergencyPhoneNumber[]> {
         return this._http
                     .get('http://83.212.115.201/api.php/country/emergency/' + country)
-                    .map((response: Response) => response.json());
+                    .map((response: Response) => <IEmergencyPhoneNumber[]>response.json());
     }
 
 }
