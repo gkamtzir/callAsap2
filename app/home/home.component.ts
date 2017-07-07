8c10ff233972ae4a0bf7f2336a8f09eafce4c21a
George Kamtziridis <georgekam96@gmail.com>
2017-07-07 14:01:15 +0300

    Updated the home page

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountryService } from '../shared/country.service';
+import { ICountry, ICountryName, IEmergencyPhoneNumber } from '../shared/interfaces/country';
 
 @Component({
     moduleId: module.id,
     templateUrl: 'home.component.html'
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+    countryName: ICountryName;
+    country: ICountry;
+    emergencyPhoneNumbers: IEmergencyPhoneNumber[]
 
     constructor(private _countryService: CountryService){}
+
+    ngOnInit(): void {
+
+        this._countryService.getCountryName()
+            .subscribe(
+                countryName => {
+
+                    this.countryName = countryName;
+                    this._countryService.getCountryString(this.countryName.country_name)
+                        .subscribe(
+                            country => {
+                                this.country = country;
+                                this._countryService.getEmergencyPhoneNumbers(this.country)
+                                    .subscribe(
+                                        emergencyPhoneNumbers => this.emergencyPhoneNumbers = emergencyPhoneNumbers
+                                    );
+                             }
+                        )
+
+                }
+            )
+
+    }
 
 }
