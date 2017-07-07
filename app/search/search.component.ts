5b2652efdec1dd4dfc8dfe2b6f538772eb3c54c9
George Kamtziridis <georgekam96@gmail.com>
2017-07-07 13:36:13 +0300

    Updated the search page

--- app/search/search.component.ts
+++ app/search/search.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountryService } from '../shared/country.service';
+import { ICountry, IEmergencyPhoneNumber } from '../shared/interfaces/country';
 
 @Component({
     moduleId: module.id,
     templateUrl: 'search.component.html'
 })
 
-export class SearchComponent {
+export class SearchComponent implements OnInit{
+
+    countries: ICountry[];
+    emergencyPhoneNumbers: IEmergencyPhoneNumber[];
+    country: ICountry;
+
 
     constructor(private _countryService: CountryService) {}
+
+    ngOnInit(): void {
+
+        this._countryService.getCountries()
+            .subscribe(
+                countries => this.countries = countries
+            );
+
+    }
+
+    changeCountry(country: ICountry): void {
+
+        this._countryService.getEmergencyPhoneNumbers(country)
+            .subscribe(
+                emergencyPhoneNumbers => this.emergencyPhoneNumbers = emergencyPhoneNumbers
+            );
+
+    }
 
 }
