535d5b7acc35d6faf99f087d3a200d880302e1ff
George Kamtziridis <georgekam96@gmail.com>
2017-07-07 14:00:20 +0300

    Altered the ICountryName interface

--- app/shared/interfaces/country.ts
+++ app/shared/interfaces/country.ts
@@ -15,18 +15,15 @@ export interface IEmergencyPhoneNumber {
 }
 
 export interface ICountryName {
-    as: string;
+    ip: string;
+    country_code: string;
+    country_name: string;
+    region_code: string;
+    region_name: string;
     city: string;
-    country: string;
-    countryCode: string;
-    isp: string;
-    lat: number;
-    lon: number;
-    org: string;
-    query: string;
-    region: string;
-    regionName: string;
-    status: string;
-    timezone: string;
-    zip: string;
+    zip_code: string;
+    time_zone: string;
+    latitude: number;
+    longitude: number;
+    metro_code: number;
 }
